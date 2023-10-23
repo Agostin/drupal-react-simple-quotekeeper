58423e6e01590cd07e56f364c02330f4fc6f4ba2
Agostin <agocnv93@gmail.com>
2023-10-23 16:22:14 +0200

    feat: add keywords search feature

--- web/themes/custom/quoter/js/src/components/Quotes/QuotesList.tsx
+++ web/themes/custom/quoter/js/src/components/Quotes/QuotesList.tsx
@@ -7,6 +7,7 @@ import Quote from './Quote'
 const QuotesList = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [quotes, setQuotes] = useState<Array<IQuote>>([])
+  const [filteredQuotes, setFilteredQuotes] = useState<Array<IQuote>>([])
   const [error, setError] = useState<string>('')
 
   const fetchStoredQuotes = async () => {
@@ -21,11 +22,17 @@ const QuotesList = () => {
       const data = await response.json()
 
       if (response.status === 200) {
-        setQuotes(typeof data.content === 'string' ? JSON.parse(data.content) : data.content)
+        const list = typeof data.content === 'string' ? JSON.parse(data.content) : data.content
+        setQuotes(list)
+        setFilteredQuotes(list)
       } else {
         setError('Error')
+        setQuotes([])
+        setFilteredQuotes([])
       }
     } catch (e) {
+      setQuotes([])
+      setFilteredQuotes([])
       setError('Error')
     }
     setIsLoading(false)
@@ -37,14 +44,32 @@ const QuotesList = () => {
     setQuotes([ quote, ...quotes ])
   }
 
+  const handleKeyboardSearch = (evt: any) => {
+    const keyword = evt.target.value
+
+    if (keyword.length > 2) {
+      const filteredQuotes = quotes.filter(
+        (q: IQuote) => q.content.indexOf(keyword) !== -1 || q.author.indexOf(keyword) !== -1
+      )
+      setFilteredQuotes(filteredQuotes)
+    } else {
+      setFilteredQuotes(quotes)
+    }
+  }
+
   useEffect(() => {
     fetchStoredQuotes()
   }, [])
 
   return (
-    <>
-      <h1 className='text-3xl mb-4'>Quotes List</h1>
+    <div className='container'>
+      <div className='flex items-center flex-col justify-between lg:flex-row'>
+        <h1 className='text-3xl mb-4'>A cool quotes keeper</h1>
+        <input type="search" onChange={handleKeyboardSearch} placeholder="Search for keyword..." className='hidden sm:flex items-center w-72 text-left space-x-3 px-4 h-12 bg-white ring-1 ring-slate-900/10 hover:ring-slate-300 focus:outline-none focus:ring-2 focus:ring-sky-500 shadow-sm rounded-lg text-slate-400' />
+      </div>
+
       <QuoteForm onNewNoteSubmitted={(newQuote: IQuote) => onNewNoteSubmittedHandler(newQuote)} />
+
       {
         isLoading ?
           <p>Loading...</p> :
@@ -52,8 +77,9 @@ const QuotesList = () => {
             {
               error ||
               <>
+
                 <ul className='grid gap-4 grid-span-full md:grid-cols-2 lg:grid-cols-3'>
-                  {quotes.map(({ author, content }, i) => {
+                  {filteredQuotes.map(({ author, content }, i) => {
                     const key = `quote__${author.toLowerCase().replace(/ /g, '_')}__${i}`
 
                     return <Quote key={key} {...{ author, content }} />
@@ -63,7 +89,7 @@ const QuotesList = () => {
             }
           </>
       }
-    </>
+    </div>
   )
 }
 
--- web/themes/custom/quoter/js/src/index.tsx
+++ web/themes/custom/quoter/js/src/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'
 
 import QuotesList from './components/Quotes/QuotesList'
-import QuoteSuggester from './components/Quotes/QuoteSuggester';
+// import QuoteSuggester from './components/Quotes/QuoteSuggester';
 
 ReactDOM.createRoot(
   document.getElementById('react-app') as HTMLElement).render(
     <React.StrictMode>
       <>
-        <QuoteSuggester />
+        {/* <QuoteSuggester /> */}
         <QuotesList />
       </>
     </React.StrictMode>,
