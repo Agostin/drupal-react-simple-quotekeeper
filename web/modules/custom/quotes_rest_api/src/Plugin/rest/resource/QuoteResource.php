42f6be9e4d57f9660f988ebcbf2399d2b989537e
Agostin <agocnv93@gmail.com>
2023-10-23 14:12:03 +0200

    feat: add GET method support on QuoteResosurce API add POST method support on QuoteResosurce API

--- web/modules/custom/quotes_rest_api/src/Plugin/rest/resource/QuoteResource.php
+++ web/modules/custom/quotes_rest_api/src/Plugin/rest/resource/QuoteResource.php
@@ -5,24 +5,30 @@ namespace Drupal\quotes_rest_api\Plugin\rest\resource;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
 
+use Symfony\Component\HttpFoundation\Request;
+
 /**
- * Provides a Demo Resource
+ * Provides a Quote Resource
  *
  * @RestResource(
  *   id = "quote_resource",
- *   label = @Translation("Demo Resource"),
+ *   label = @Translation("Quote Resource"),
  *   uri_paths = {
- *     "canonical" = "/quotes_rest_api/demo_resource"
+ *     "canonical" = "/api/quotes",
+ *     "create" = "/api/quote/new"
  *   }
  * )
  */
 class QuoteResource extends ResourceBase {
-/**
+  /**
    * Responds to entity GET requests.
    * @return \Drupal\rest\ResourceResponse
    */
-  public function get() {
-    $response = ['message' => 'Hello, this is a rest service'];
+   public function get() {
+    $query = \Drupal::database()->query("SELECT id, content, author FROM {quotes} ORDER BY id DESC");
+    $result = $query->fetchAll();
+    $response = ['content' => json_encode($result)];
+
     return new ResourceResponse($response);
   }
 
@@ -30,13 +36,31 @@ class QuoteResource extends ResourceBase {
    * Responds to entity POST requests.
    * @return \Drupal\rest\ResourceResponse
    */
-  // public function post() {}
+  public function post(Request $request) {
+    $data = json_decode($request->getContent());
+
+    $result = \Drupal::database()
+      ->insert('quotes')
+      ->fields([
+        'author' => $data->author,
+        'content' => $data->content,
+      ])
+      ->execute();
+
+    return new ResourceResponse(['content' => $response]);
+  }
+
+  /**
+   * Responds to entity DELETE requests.
+   * @return \Drupal\rest\ResourceResponse
+   */
+  // public function delete() {}
 
   /**
    * Suggests random quotes.
    * @return \Drupal\rest\ResourceResponse
    */
-  public function getRandomQuote() {
+  public function getRandomQuotes() {
     try {
       $client = new Client();
       $guzzleResponse = $client->get("https://type.fit/api/quotes");
