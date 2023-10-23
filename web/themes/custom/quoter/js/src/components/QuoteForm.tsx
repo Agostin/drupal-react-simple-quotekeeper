88d0348e0417773651fa6a6c2b7f37a82498ed17
Agostin <agocnv93@gmail.com>
2023-10-23 11:29:15 +0200

    fix(WIP):update UI

--- web/themes/custom/quoter/js/src/components/QuoteForm.tsx
+++ web/themes/custom/quoter/js/src/components/QuoteForm.tsx
@@ -3,26 +3,23 @@ import { useState } from 'react';
 const QuoteForm = () => {
   const [newQuote, setNewQuote] = useState({
     content: '',
-    author: '',
-    tags: [],
+    author: ''
   });
 
-  const handleChange = (e: any) => {
-    const { name, value } = e.target;
-
+  const handleChange = (evt: any, field: 'author' | 'content') => {
+    const value = evt.target.value
     setNewQuote({
       ...newQuote,
-      [name]: name === 'tags' ? value.split(',').map((tag: string) => tag) : value,
-    });
-  };
+      [field]: value
+    })
+  }
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
 
     setNewQuote({
       content: '',
-      author: '',
-      tags: [],
+      author: ''
     });
 
     // Store new quote on MySQL db
@@ -37,7 +34,7 @@ const QuoteForm = () => {
           id="content"
           name="content"
           value={newQuote.content}
-          onChange={handleChange}
+          onChange={() => handleChange(event, 'content')}
           className="w-full p-2 border rounded-md"
           required
         />
@@ -49,20 +46,9 @@ const QuoteForm = () => {
           name="author"
           type="text"
           value={newQuote.author}
-          onChange={handleChange}
+          onChange={() => handleChange(event, 'author')}
           className="w-full p-2 border rounded-md"
           required
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="tags" className="block text-lg font-semibold mb-2">Tags (comma-separated)</label>
-        <input
-          id="tags"
-          name="tags"
-          type="text"
-          value={newQuote.tags.join(',')}
-          onChange={handleChange}
-          className="w-full p-2 border rounded-md"
         />
       </div>
       <button type="submit" className="bg-blue-500 text-white rounded-full px-4 py-2">Add Quote</button>
--- web/themes/custom/quoter/js/src/components/QuotesList.tsx
+++ web/themes/custom/quoter/js/src/components/QuotesList.tsx
@@ -4,14 +4,34 @@ import { IQuote } from '../../types';
 import Quote from './Quote'
 
 const QuotesList = () => {
-  const [quotes, setQuotes] = useState<Array<IQuote>>([])
+  const [sessionToken, setSessionToken] = useState<string>('')
   const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [quotes, setQuotes] = useState<Array<IQuote>>([])
   const [error, setError] = useState<string>('')
 
-  const suggestQuote = async () => {
+  const retrieveSessionToken = async () => {
+    try {
+      const response = await fetch('/session/token')
+      const token = await response.text()
+
+      if (response.status === 200 && token) {
+        setSessionToken(token)
+      }
+    } catch (e) {
+      setError('Error')
+    }
+  }
+
+  const fetchStoredQuotes = async () => {
     setIsLoading(true)
     try {
-      const response = await fetch('/quotes_rest_api/quotes_resource');
+      const response = await fetch('/quotes_rest_api/demo_resource?_format=json', {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRF-Token': sessionToken,
+          'Authorization': 'Basic QWdvc3Rpbm86c3BhcmtmYWJyaWs='
+        }
+      });
       const data = await response.json()
       console.log(data)
 
@@ -26,13 +46,40 @@ const QuotesList = () => {
     setIsLoading(false)
   }
 
-  const fetchStoredQuotes = async () => {
-    // Fetch quotes from MySQL database
+  const suggestQuotes = async () => {
+    setIsLoading(true)
+    try {
+      const response = await fetch('/api/quotes/random?_format=json', {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-CSRF-Token': sessionToken
+        }
+      });
+      const data = await response.json()
+      console.log(data)
+
+      if (response.status === 200) {
+        setQuotes(data.content)
+      } else {
+        setError('Error')
+      }
+    } catch (e) {
+      setError('Error')
+    }
+    setIsLoading(false)
   }
 
   useEffect(() => {
-    fetchStoredQuotes()
+    if (!sessionToken) {
+      retrieveSessionToken()
+    }
   }, [])
+
+  useEffect(() => {
+    if (sessionToken) {
+      fetchStoredQuotes()
+    }
+  }, [sessionToken])
 
   return (
     <>
@@ -45,7 +92,7 @@ const QuotesList = () => {
             {
               error ||
               <>
-                <button onClick={suggestQuote} className="bg-blue-500 text-white rounded-full px-4 py-2">Suggest quote</button>
+                <button onClick={suggestQuotes} className="bg-blue-500 text-white rounded-full px-4 py-2">Suggest quote</button>
                 <ul className='grid gap-4 grid-span-full md:grid-cols-2 lg:grid-cols-3'>
                   {quotes.map(({ author, text }, i) => {
                     const key = `quote__${author.toLowerCase().replace(/ /g, '_')}__${i}`
